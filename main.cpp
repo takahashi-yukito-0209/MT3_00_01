95826879f01fd509e70c5a5142a30e415ee73d70
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-05-09 14:50:06 +0900

    10_00の課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,105 @@
 #include <Novice.h>
+#include <assert.h>
+#include <cmath>
 
-const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_00_";
+const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_01_00";
+
+struct Matrix4x4 {
+    float m[4][4];
+};
+
+struct Vector3 {
+    float x, y, z;
+};
+
+// 透視投影行列（透視変換行列）
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+    Matrix4x4 result = {};
+
+    result.m[0][0] = 1.0f / aspectRatio * (1.0f / tanf(fovY/ 2.0f));
+    result.m[0][1] = 0.0f;
+    result.m[0][2] = 0.0f;
+    result.m[0][3] = 0.0f;
+    result.m[1][0] = 0.0f;
+    result.m[1][1] = 1.0f / tanf(fovY / 2.0f);
+    result.m[1][2] = 0.0f;
+    result.m[1][3] = 0.0f;
+    result.m[2][0] = 0.0f;
+    result.m[2][1] = 0.0f;
+    result.m[2][2] = farClip / (farClip - nearClip);
+    result.m[2][3] = 1.0f;
+    result.m[3][0] = 0.0f;
+    result.m[3][1] = 0.0f;
+    result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+    result.m[3][3] = 0.0f;
+
+    return result;
+}
+
+// 正射影行列
+Matrix4x4 MakeOrthograhicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip)
+{
+    Matrix4x4 result = {};
+
+    result.m[0][0] = 2.0f / (right - left);
+    result.m[0][1] = 0.0f;
+    result.m[0][2] = 0.0f;
+    result.m[0][3] = 0.0f;
+    result.m[1][0] = 0.0f;
+    result.m[1][1] = 2.0f / (top - bottom);
+    result.m[1][2] = 0.0f;
+    result.m[1][3] = 0.0f;
+    result.m[2][0] = 0.0f;
+    result.m[2][1] = 0.0f;
+    result.m[2][2] = 1.0f / (farClip - nearClip);
+    result.m[2][3] = 0.0f;
+    result.m[3][0] = (left + right) / (left - right);
+    result.m[3][1] = (top + bottom) / (bottom - top);
+    result.m[3][2] = nearClip / (nearClip - farClip);
+    result.m[3][3] = 1.0f;
+
+    return result;
+}
+
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
+{
+    Matrix4x4 result = {};
+
+    result.m[0][0] = width / 2.0f;
+    result.m[0][1] = 0.0f;
+    result.m[0][2] = 0.0f;
+    result.m[0][3] = 0.0f;
+    result.m[1][0] = 0.0f;
+    result.m[1][1] = -(height / 2.0f);
+    result.m[1][2] = 0.0f;
+    result.m[1][3] = 0.0f;
+    result.m[2][0] = 0.0f;
+    result.m[2][1] = 0.0f;
+    result.m[2][2] = maxDepth - minDepth;
+    result.m[2][3] = 0.0f;
+    result.m[3][0] = left + (width / 2.0f);
+    result.m[3][1] = top + (height / 2.0f);
+    result.m[3][2] = minDepth;
+    result.m[3][3] = 1.0f;
+
+    return result;
+}
+
+static const int kRowHeight = 25;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label)
+{
+    Novice::ScreenPrintf(x, y, "%s", label);
+
+    for (int row = 0; row < 4; row++) {
+        for (int column = 0; column < 4; column++) {
+            Novice::ScreenPrintf(x + column * kColumnWidth, y + 30 + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+        }
+    }
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -25,6 +124,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         ///
         /// ↓更新処理ここから
         ///
+
+        Matrix4x4 orthographicMatrix = MakeOrthograhicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+        Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+        Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+
+        MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+        MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+        MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
         ///
         /// ↑更新処理ここまで

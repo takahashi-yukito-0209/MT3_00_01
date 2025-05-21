824f9a2c133df912658d4af37f08b9e00950e4f9
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-05-21 15:01:41 +0900

    スフィアとグリット線表示成功

--- main.cpp
+++ main.cpp
@@ -1,17 +1,454 @@
 #include <Novice.h>
+#include <assert.h>
+#include <cmath>
+#include <cstdint>
+#include <imgui.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_00_";
+const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_01_02";
+
+struct Vector3 {
+    float x, y, z;
+};
+
+struct Matrix4x4 {
+    float m[4][4];
+};
+
+struct Sphere {
+    Vector3 center; //!< 中心点
+    float radius; //!< 半径
+};
+
+// 4.逆行列
+Matrix4x4 Inverse(const Matrix4x4& m)
+{
+    Matrix4x4 result = {};
+
+    float det = m.m[0][0] * m.m[1][1] * m.m[2][2] * m.m[3][3] + m.m[0][0] * m.m[1][2] * m.m[2][3] * m.m[3][1] + m.m[0][0] * m.m[1][3] * m.m[2][1] * m.m[3][2] - m.m[0][0] * m.m[1][3] * m.m[2][2] * m.m[3][1] - m.m[0][0] * m.m[1][2] * m.m[2][1] * m.m[3][3] - m.m[0][0] * m.m[1][1] * m.m[2][3] * m.m[3][2] - m.m[0][1] * m.m[1][0] * m.m[2][2] * m.m[3][3] - m.m[0][2] * m.m[1][0] * m.m[2][3] * m.m[3][1] - m.m[0][3] * m.m[1][0] * m.m[2][1] * m.m[3][2] + m.m[0][3] * m.m[1][0] * m.m[2][2] * m.m[3][1] + m.m[0][2] * m.m[1][0] * m.m[2][1] * m.m[3][3] + m.m[0][1] * m.m[1][0] * m.m[2][3] * m.m[3][2] + m.m[0][1] * m.m[1][2] * m.m[2][0] * m.m[3][3] + m.m[0][2] * m.m[1][3] * m.m[2][0] * m.m[3][1] + m.m[0][3] * m.m[1][1] * m.m[2][0] * m.m[3][2] - m.m[0][3] * m.m[1][2] * m.m[2][0] * m.m[3][1] - m.m[0][2] * m.m[1][1] * m.m[2][0] * m.m[3][3] - m.m[0][1] * m.m[1][3] * m.m[2][0] * m.m[3][2] - m.m[0][1] * m.m[1][2] * m.m[2][3] * m.m[3][0] - m.m[0][2] * m.m[1][3] * m.m[2][1] * m.m[3][0] - m.m[0][3] * m.m[1][1] * m.m[2][2] * m.m[3][0] + m.m[0][3] * m.m[1][2] * m.m[2][1] * m.m[3][0] + m.m[0][2] * m.m[1][1] * m.m[2][3] * m.m[3][0] + m.m[0][1] * m.m[1][3] * m.m[2][2] * m.m[3][0];
+
+    if (det == 0.0f) {
+        return result;
+    }
+
+    float invDet = 1.0f / det;
+
+    result.m[0][0] = invDet * (m.m[1][1] * m.m[2][2] * m.m[3][3] + m.m[1][2] * m.m[2][3] * m.m[3][1] + m.m[1][3] * m.m[2][1] * m.m[3][2] - m.m[1][3] * m.m[2][2] * m.m[3][1] - m.m[1][2] * m.m[2][1] * m.m[3][3] - m.m[1][1] * m.m[2][3] * m.m[3][2]);
+
+    result.m[0][1] = invDet * (-m.m[0][1] * m.m[2][2] * m.m[3][3] - m.m[0][2] * m.m[2][3] * m.m[3][1] - m.m[0][3] * m.m[2][1] * m.m[3][2] + m.m[0][3] * m.m[2][2] * m.m[3][1] + m.m[0][2] * m.m[2][1] * m.m[3][3] + m.m[0][1] * m.m[2][3] * m.m[3][2]);
+
+    result.m[0][2] = invDet * (m.m[0][1] * m.m[1][2] * m.m[3][3] + m.m[0][2] * m.m[1][3] * m.m[3][1] + m.m[0][3] * m.m[1][1] * m.m[3][2] - m.m[0][3] * m.m[1][2] * m.m[3][1] - m.m[0][2] * m.m[1][1] * m.m[3][3] - m.m[0][1] * m.m[1][3] * m.m[3][2]);
+
+    result.m[0][3] = invDet * (-m.m[0][1] * m.m[1][2] * m.m[2][3] - m.m[0][2] * m.m[1][3] * m.m[2][1] - m.m[0][3] * m.m[1][1] * m.m[2][2] + m.m[0][3] * m.m[1][2] * m.m[2][1] + m.m[0][2] * m.m[1][1] * m.m[2][3] + m.m[0][1] * m.m[1][3] * m.m[2][2]);
+
+    result.m[1][0] = invDet * (-m.m[1][0] * m.m[2][2] * m.m[3][3] - m.m[1][2] * m.m[2][3] * m.m[3][0] - m.m[1][3] * m.m[2][0] * m.m[3][2] + m.m[1][3] * m.m[2][2] * m.m[3][0] + m.m[1][2] * m.m[2][0] * m.m[3][3] + m.m[1][0] * m.m[2][3] * m.m[3][2]);
+
+    result.m[1][1] = invDet * (m.m[0][0] * m.m[2][2] * m.m[3][3] + m.m[0][2] * m.m[2][3] * m.m[3][0] + m.m[0][3] * m.m[2][0] * m.m[3][2] - m.m[0][3] * m.m[2][2] * m.m[3][0] - m.m[0][2] * m.m[2][0] * m.m[3][3] - m.m[0][0] * m.m[2][3] * m.m[3][2]);
+
+    result.m[1][2] = invDet * (-m.m[0][0] * m.m[1][2] * m.m[3][3] - m.m[0][2] * m.m[1][3] * m.m[3][0] - m.m[0][3] * m.m[1][0] * m.m[3][2] + m.m[0][3] * m.m[1][2] * m.m[3][0] + m.m[0][2] * m.m[1][0] * m.m[3][3] + m.m[0][0] * m.m[1][3] * m.m[3][2]);
+
+    result.m[1][3] = invDet * (m.m[0][0] * m.m[1][2] * m.m[2][3] + m.m[0][2] * m.m[1][3] * m.m[2][0] + m.m[0][3] * m.m[1][0] * m.m[2][2] - m.m[0][3] * m.m[1][2] * m.m[2][0] - m.m[0][2] * m.m[1][0] * m.m[2][3] - m.m[0][0] * m.m[1][3] * m.m[2][2]);
+
+    result.m[2][0] = invDet * (m.m[1][0] * m.m[2][1] * m.m[3][3] + m.m[1][1] * m.m[2][3] * m.m[3][0] + m.m[1][3] * m.m[2][0] * m.m[3][1] - m.m[1][3] * m.m[2][1] * m.m[3][0] - m.m[1][1] * m.m[2][0] * m.m[3][3] - m.m[1][0] * m.m[2][3] * m.m[3][1]);
+
+    result.m[2][1] = invDet * (-m.m[0][0] * m.m[2][1] * m.m[3][3] - m.m[0][1] * m.m[2][3] * m.m[3][0] - m.m[0][3] * m.m[2][0] * m.m[3][1] + m.m[0][3] * m.m[2][1] * m.m[3][0] + m.m[0][1] * m.m[2][0] * m.m[3][3] + m.m[0][0] * m.m[2][3] * m.m[3][1]);
+
+    result.m[2][2] = invDet * (m.m[0][0] * m.m[1][1] * m.m[3][3] + m.m[0][1] * m.m[1][3] * m.m[3][0] + m.m[0][3] * m.m[1][0] * m.m[3][1] - m.m[0][3] * m.m[1][1] * m.m[3][0] - m.m[0][1] * m.m[1][0] * m.m[3][3] - m.m[0][0] * m.m[1][3] * m.m[3][1]);
+
+    result.m[2][3] = invDet * (-m.m[0][0] * m.m[1][1] * m.m[2][3] - m.m[0][1] * m.m[1][3] * m.m[2][0] - m.m[0][3] * m.m[1][0] * m.m[2][1] + m.m[0][3] * m.m[1][1] * m.m[2][0] + m.m[0][1] * m.m[1][0] * m.m[2][3] + m.m[0][0] * m.m[1][3] * m.m[2][1]);
+
+    result.m[3][0] = invDet * (-m.m[1][0] * m.m[2][1] * m.m[3][2] - m.m[1][1] * m.m[2][2] * m.m[3][0] - m.m[1][2] * m.m[2][0] * m.m[3][1] + m.m[1][2] * m.m[2][1] * m.m[3][0] + m.m[1][1] * m.m[2][0] * m.m[3][2] + m.m[1][0] * m.m[2][2] * m.m[3][1]);
+
+    result.m[3][1] = invDet * (m.m[0][0] * m.m[2][1] * m.m[3][2] + m.m[0][1] * m.m[2][2] * m.m[3][0] + m.m[0][2] * m.m[2][0] * m.m[3][1] - m.m[0][2] * m.m[2][1] * m.m[3][0] - m.m[0][1] * m.m[2][0] * m.m[3][2] - m.m[0][0] * m.m[2][2] * m.m[3][1]);
+
+    result.m[3][2] = invDet * (-m.m[0][0] * m.m[1][1] * m.m[3][2] - m.m[0][1] * m.m[1][2] * m.m[3][0] - m.m[0][2] * m.m[1][0] * m.m[3][1] + m.m[0][2] * m.m[1][1] * m.m[3][0] + m.m[0][1] * m.m[1][0] * m.m[3][2] + m.m[0][0] * m.m[1][2] * m.m[3][1]);
+
+    result.m[3][3] = invDet * (m.m[0][0] * m.m[1][1] * m.m[2][2] + m.m[0][1] * m.m[1][2] * m.m[2][0] + m.m[0][2] * m.m[1][0] * m.m[2][1] - m.m[0][2] * m.m[1][1] * m.m[2][0] - m.m[0][1] * m.m[1][0] * m.m[2][2] - m.m[0][0] * m.m[1][2] * m.m[2][1]);
+
+    return result;
+}
+
+// 3.行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+    Matrix4x4 result = {};
+
+    for (int row = 0; row < 4; row++) {
+        for (int column = 0; column < 4; column++) {
+
+            result.m[row][column] = 0;
+
+            for (int k = 0; k < 4; k++) {
+                result.m[row][column] += m1.m[row][k] * m2.m[k][column];
+            }
+        }
+    }
+
+    return result;
+}
+
+// 1.平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        1.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        translate.x,
+        translate.y,
+        translate.z,
+        1.0f,
+    };
+
+    return result;
+};
+
+// 2.拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        scale.x,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        scale.y,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        scale.z,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+    };
+
+    return result;
+};
+
+// 3.座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
+{
+    Vector3 result;
+    result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + matrix.m[3][0];
+    result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + matrix.m[3][1];
+    result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + matrix.m[3][2];
+    float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + matrix.m[3][3];
+    assert(w != 0.0f);
+    result.x /= w;
+    result.y /= w;
+    result.z /= w;
+
+    return result;
+};
+
+// 1.X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        1.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        std::cos(radian),
+        std::sin(radian),
+        0.0f,
+        0.0f,
+        std::sin(-radian),
+        std::cos(radian),
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+
+    return result;
+};
+
+// 2.Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        std::cos(radian),
+        0.0f,
+        std::sin(-radian),
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        std::sin(radian),
+        0.0f,
+        std::cos(radian),
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+
+    return result;
+};
+
+// 3.Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        std::cos(radian),
+        std::sin(radian),
+        0.0f,
+        0.0f,
+        std::sin(-radian),
+        std::cos(radian),
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+
+    return result;
+};
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate)
+{
+
+    // 回転軸をひとつにまとめる（合成）
+    Matrix4x4 rotateXYZMatrix = Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
+
+    // 回転と拡縮、移動をすべて合成
+    Matrix4x4 result = Multiply(Multiply(MakeScaleMatrix(scale), rotateXYZMatrix), MakeTranslateMatrix(translate));
+
+    return result;
+};
+
+// 透視投影行列（透視変換行列）
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+    Matrix4x4 result = {};
+
+    result.m[0][0] = 1.0f / aspectRatio * (1.0f / tanf(fovY / 2.0f));
+    result.m[0][1] = 0.0f;
+    result.m[0][2] = 0.0f;
+    result.m[0][3] = 0.0f;
+    result.m[1][0] = 0.0f;
+    result.m[1][1] = 1.0f / tanf(fovY / 2.0f);
+    result.m[1][2] = 0.0f;
+    result.m[1][3] = 0.0f;
+    result.m[2][0] = 0.0f;
+    result.m[2][1] = 0.0f;
+    result.m[2][2] = farClip / (farClip - nearClip);
+    result.m[2][3] = 1.0f;
+    result.m[3][0] = 0.0f;
+    result.m[3][1] = 0.0f;
+    result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+    result.m[3][3] = 0.0f;
+
+    return result;
+}
+
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
+{
+    Matrix4x4 result = {};
+
+    result.m[0][0] = width / 2.0f;
+    result.m[0][1] = 0.0f;
+    result.m[0][2] = 0.0f;
+    result.m[0][3] = 0.0f;
+    result.m[1][0] = 0.0f;
+    result.m[1][1] = -(height / 2.0f);
+    result.m[1][2] = 0.0f;
+    result.m[1][3] = 0.0f;
+    result.m[2][0] = 0.0f;
+    result.m[2][1] = 0.0f;
+    result.m[2][2] = maxDepth - minDepth;
+    result.m[2][3] = 0.0f;
+    result.m[3][0] = left + (width / 2.0f);
+    result.m[3][1] = top + (height / 2.0f);
+    result.m[3][2] = minDepth;
+    result.m[3][3] = 1.0f;
+
+    return result;
+}
+
+static const int kRowHeight = 25;
+static const int kColumnWidth = 60;
+
+static const int kWindowWidth = 1280;
+static const int kWindowHeight = 720;
+
+// 成分の数値表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
+{
+    Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+    Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+    Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+    Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+// 4x4行列の数値表示
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label)
+{
+    Novice::ScreenPrintf(x, y, "%s", label);
+
+    for (int row = 0; row < 4; row++) {
+        for (int column = 0; column < 4; column++) {
+            Novice::ScreenPrintf(x + column * kColumnWidth, y + 30 + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+        }
+    }
+}
+
+// クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2)
+{
+    Vector3 result = {};
+
+    result.x = v1.y * v2.z - v1.z * v2.y;
+    result.y = v1.z * v2.x - v1.x * v2.z;
+    result.z = v1.x * v2.y - v1.y * v2.x;
+
+    return result;
+}
+
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMattrix)
+{
+
+    const float kGridHalfWidth = 2.0f; // Gridの半分の幅
+    const uint32_t kSubdivision = 10; // 分割数
+    const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision); // 1つ分の長さ
+
+    // 奥から手前への線を順々に引いていく
+    for (uint32_t xIndex = 0; xIndex <= kSubdivision; xIndex++) {
+        
+        // 上の情報を使ってワールド座標系上の始点と終点を求める
+        Vector3 start = { float(-(kSubdivision / 2.0f) + xIndex) * kGridEvery, 0.0f, float(-kGridHalfWidth) };
+        Vector3 end = { float(-(kSubdivision / 2.0f) + xIndex) * kGridEvery, 0.0f, float(kGridHalfWidth) };
+        
+        // スクリーン座標系まで変換をかける
+        start = Transform(Transform(start, viewProjectionMatrix), viewportMattrix);
+        end = Transform(Transform(end, viewProjectionMatrix), viewportMattrix);
+
+        // 変換した座標を使って表示。色は薄い灰色(0xAAAAAAFF),原点は黒ぐらいがよいが、なんでも良い
+        Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), 0xAAAAAAFF);
+    }
+
+    // 左から右も同じように順々に引いていく
+    for (uint32_t zIndex = 0; zIndex <= kSubdivision; zIndex++) {
+        
+        // 奥から手前が左右に変わるだけ
+        Vector3 start = { float(-kGridHalfWidth), 0.0f, float(-(kSubdivision / 2.0f) + zIndex) * kGridEvery };
+        Vector3 end = { float(kGridHalfWidth), 0.0f, float(-(kSubdivision / 2.0f) + zIndex) * kGridEvery };
+        
+        // スクリーン座標系まで変換をかける
+        start = Transform(Transform(start, viewProjectionMatrix), viewportMattrix);
+        end = Transform(Transform(end, viewProjectionMatrix), viewportMattrix);
+
+        // 変換した座標を使って表示。色は薄い灰色(0xAAAAAAFF),原点は黒ぐらいがよいが、なんでも良い
+        Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), 0xAAAAAAFF);
+    }
+}
+
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+    const uint32_t kSubdivision = 10; // 分割数
+    const float kLonEvery = 2.0f * float(M_PI) / kSubdivision; // 経度分割1つ分の角度
+    const float kLatEvery = float(M_PI) / kSubdivision; // 緯度分割1つ分の角度
+    // 緯度の方向に分割　-π/2 ~ π/2
+    for (uint32_t latIndex = 0; latIndex < kSubdivision; latIndex++) {
+        float lat = -float(M_PI) / 2.0f + kLatEvery * latIndex; // 現在の緯度
+        // 経度の方向に分割　0 ~ 2π
+        for (uint32_t lonIndex = 0; lonIndex < kSubdivision; lonIndex++) {
+            float lon = lonIndex * kLonEvery; // 現在の経度
+            // world座標系でのa,b,cを求める
+            Vector3 a, b, c;
+            a = { cosf(lat) * cosf(lon), sinf(lat), cosf(lat) * sinf(lon) };
+            b = { cosf(lat + kLatEvery) * cosf(lon), sinf(lat + kLatEvery), cosf(lat + kLatEvery) * sinf(lon) };
+            c = { cosf(lat) * cosf(lon + kLonEvery), sinf(lat), cosf(lat) * sinf(lon + kLonEvery) };
+
+            a.x = a.x * sphere.radius + sphere.center.x;
+            a.y = a.y * sphere.radius + sphere.center.y;
+            a.z = a.z * sphere.radius + sphere.center.z;
+
+            b.x = b.x * sphere.radius + sphere.center.x;
+            b.y = b.y * sphere.radius + sphere.center.y;
+            b.z = b.z * sphere.radius + sphere.center.z;
+
+            c.x = c.x * sphere.radius + sphere.center.x;
+            c.y = c.y * sphere.radius + sphere.center.y;
+            c.z = c.z * sphere.radius + sphere.center.z;
+
+            // a,b,c,をScreen座標系まで変換
+            a = Transform(a, viewProjectionMatrix);
+            a = Transform(a, viewportMatrix);
+            b = Transform(b, viewProjectionMatrix);
+            b = Transform(b, viewportMatrix);
+            c = Transform(c, viewProjectionMatrix);
+            c = Transform(c, viewportMatrix);
+
+            // ab,bcで線を引く
+            Novice::DrawLine(int(a.x), int(a.y), int(b.x), int(b.y), color);
+            Novice::DrawLine(int(b.x), int(b.y), int(c.x), int(c.y), color);
+        }
+    }
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 
     // ライブラリの初期化
-    Novice::Initialize(kWindowTitle, 1280, 720);
+    Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
     // キー入力結果を受け取る箱
     char keys[256] = { 0 };
     char preKeys[256] = { 0 };
+
+    Vector3 rotate = {};
+    Vector3 translate = {};
+
+    unsigned int color = 0xAAAAAAFF;
+
+    // カメラの位置と角度
+    Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
+    Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
+    Vector3 cameraPosition = { 0.0f, 0.0f, 10.0f };
+
+    Sphere sphere;
+    sphere.center.x = 0.0f;
+    sphere.center.y = -2.0f;
+    sphere.center.z = 0.0f;
+    sphere.radius = 1.0f;
 
     // ウィンドウの×ボタンが押されるまでループ
     while (Novice::ProcessMessage() == 0) {
@@ -26,6 +463,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         /// ↓更新処理ここから
         ///
 
+        Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
+        Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate, cameraPosition);
+        Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+        Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+        Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+        Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
         ///
         /// ↑更新処理ここまで
         ///
@@ -33,6 +477,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         ///
         /// ↓描画処理ここから
         ///
+
+        DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+        DrawSphere(sphere, worldViewProjectionMatrix, viewportMatrix, color);
 
         ///
         /// ↑描画処理ここまで

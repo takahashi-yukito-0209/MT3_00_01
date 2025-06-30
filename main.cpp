5c475e4215c2060630189955f2cb0dac63eb75a4
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-06-30 12:45:12 +0900

    フォーマット生成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_00_";
+const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_02_03";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)

948da22459cbff362b1171fc979472670e9ccd46
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-07-25 15:30:10 +0900

    実装完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,169 @@
+#define NOMINMAX
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <assert.h>
+#include <cmath>
+#include <cstdint>
+#include <imgui.h>
+#include <math.h>
 
-const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_00_";
+const char kWindowTitle[] = "LE2C_17_タカハシ_ユキト_MT3_00_02";
+
+struct Vector3 {
+    float x, y, z;
+
+    // ベクトルの加算
+    Vector3 operator+(const Vector3& rhs) const
+    {
+        return { x + rhs.x, y + rhs.y, z + rhs.z };
+    }
+
+    // ベクトルの減算
+    Vector3 operator-(const Vector3& rhs) const
+    {
+        return { x - rhs.x, y - rhs.y, z - rhs.z };
+    }
+
+    // スカラー倍（右側から）
+    Vector3 operator*(float scalar) const
+    {
+        return { x * scalar, y * scalar, z * scalar };
+    }
+
+    // 符号反転（-vec）
+    Vector3 operator-() const
+    {
+        return { -x, -y, -z };
+    }
+};
+
+// スカラー倍（左側から）
+inline Vector3 operator*(float scalar, const Vector3& vec)
+{
+    return vec * scalar;
+}
+
+struct Matrix4x4 {
+    float m[4][4];
+
+    // 行列の加算
+    Matrix4x4 operator+(const Matrix4x4& rhs) const
+    {
+        Matrix4x4 result {};
+        for (int i = 0; i < 4; ++i)
+            for (int j = 0; j < 4; ++j)
+                result.m[i][j] = m[i][j] + rhs.m[i][j];
+        return result;
+    }
+
+    // 行列の減算
+    Matrix4x4 operator-(const Matrix4x4& rhs) const
+    {
+        Matrix4x4 result {};
+        for (int i = 0; i < 4; ++i)
+            for (int j = 0; j < 4; ++j)
+                result.m[i][j] = m[i][j] - rhs.m[i][j];
+        return result;
+    }
+
+    // 行列の積
+    Matrix4x4 operator*(const Matrix4x4& rhs) const
+    {
+        Matrix4x4 result {};
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                result.m[i][j] = 0.0f;
+                for (int k = 0; k < 4; ++k) {
+                    result.m[i][j] += m[i][k] * rhs.m[k][j];
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// 1.X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        1.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        std::cos(radian),
+        std::sin(radian),
+        0.0f,
+        0.0f,
+        std::sin(-radian),
+        std::cos(radian),
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+
+    return result;
+};
+
+// 2.Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        std::cos(radian),
+        0.0f,
+        std::sin(-radian),
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        std::sin(radian),
+        0.0f,
+        std::cos(radian),
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+
+    return result;
+};
+
+// 3.Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian)
+{
+    Matrix4x4 result = {};
+
+    result = {
+        std::cos(radian),
+        std::sin(radian),
+        0.0f,
+        0.0f,
+        std::sin(-radian),
+        std::cos(radian),
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+
+    return result;
+};
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -25,6 +188,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         ///
         /// ↓更新処理ここから
         ///
+
+            Vector3 a { 0.2f, 1.0f, 0.0f };
+        Vector3 b { 2.4f, 3.1f, 1.2f };
+        Vector3 c = a + b;
+        Vector3 d = a - b;
+        Vector3 e = a * 2.4f;
+        Vector3 rotate { 0.4f, 1.43f, -0.8f };
+        Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+        Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+        Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+        Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+
+        ImGui::Begin("Window");
+        ImGui::Text("c:%f, %f, %f", c.x, c.y, c.z);
+        ImGui::Text("d:%f, %f, %f", d.x, d.y, d.z);
+        ImGui::Text("e:%f, %f, %f", e.x, e.y, e.z);
+        ImGui::Text("matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n",
+            rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
+            rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
+            rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+            rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+
+        ImGui::End();
 
         ///
         /// ↑更新処理ここまで
